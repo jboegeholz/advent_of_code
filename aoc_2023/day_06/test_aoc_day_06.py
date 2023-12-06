bfedfb02981e2dadcefe4194a2c5cac70b422caa
jboegeholz <boegeholz.joern@gmail.com>
2023-12-06 09:42:29 +0100

    add day 06

---
+++ aoc_2023/day_06/test_aoc_day_06.py
@@ -0,0 +1,14 @@
+import pytest
+data="""Time:      7  15   30
+Distance:  9  40  200"""
+
+@pytest.mark.parametrize(
+    "button_pressed_for, expected_distance_travelled",
+    [
+        (1, 6),
+
+    ]
+)
+def test_seed_to_soil(button_pressed_for, expected_distance_travelled):
+    distance_travelled= 7 - button_pressed_for
+    assert distance_travelled == expected_distance_travelled
