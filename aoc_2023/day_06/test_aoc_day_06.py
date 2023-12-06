e8f784f8239c7b4ea84b26069a778b4492a66333
jboegeholz <boegeholz.joern@gmail.com>
2023-12-06 09:59:35 +0100

    solve day 06 with test data

--- aoc_2023/day_06/test_aoc_day_06.py
+++ aoc_2023/day_06/test_aoc_day_06.py
@@ -19,3 +19,20 @@ Distance:  9  40  200"""
 def test_seed_to_soil(button_pressed_for, expected_distance_travelled):
     distance_travelled = (7 - button_pressed_for) * button_pressed_for
     assert distance_travelled == expected_distance_travelled
+
+@pytest.mark.parametrize(
+    "race_time, current_record, ways_to_win",
+    [
+        (7, 9, 4),
+        (15, 40, 8),
+        (30, 200, 9),
+
+    ]
+)
+def test_seed_to_soil(race_time, current_record, ways_to_win):
+    distances = []
+    for i in range(race_time+1):
+        distance_travelled = (race_time - i) * i
+        if distance_travelled > current_record:
+            distances.append(distance_travelled)
+    assert len(distances) == ways_to_win
