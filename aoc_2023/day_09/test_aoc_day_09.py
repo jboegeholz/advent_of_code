a7e284bc1bf1f43cf2d7757f83326c05c70b86ba
jboegeholz <boegeholz.joern@gmail.com>
2023-12-09 09:20:51 +0100

    solve day 09 with test_data

--- aoc_2023/day_09/test_aoc_day_09.py
+++ aoc_2023/day_09/test_aoc_day_09.py
@@ -1,3 +1,6 @@
+data = """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
 def test_next_value():
     line = [int(c) for c in "0 3 6 9 12 15".split(" ")]
     differences = []
@@ -59,6 +62,7 @@ def test_next_value_2():
     next_value = differences[-1] + line[-1]
     assert next_value == 28
 
+
 def test_next_value_3():
     line = [int(c) for c in "10 13 16 21 30 45".split(" ")]
     all_differences = []
@@ -85,3 +89,28 @@ def test_next_value_3():
 
     next_value = all_differences[0][-1]
     assert next_value == 68
+
+def test_next_value_test_data():
+    lines = data.split("\n")
+    sum_of_extrapolated_values = 0
+    for line in lines:
+        line = [int(c) for c in line.split(" ")]
+        all_differences = []
+        all_differences.append(line)
+        # generate differences
+        while not all_zero(all_differences[-1]):
+            current_diff = all_differences[-1]
+            differences = []
+            for i in range(len(current_diff) - 1):
+                differences.append(current_diff[i + 1] - current_diff[i])
+            all_differences.append(differences)
+
+        all_differences[-1].append(0)
+
+        for i in range(len(all_differences)-1, 0, -1):
+            current_diff = all_differences[i]
+            all_differences[i-1].append(current_diff[-1] + all_differences[i-1][-1])
+
+        next_value = all_differences[0][-1]
+        sum_of_extrapolated_values += next_value
+    assert sum_of_extrapolated_values == 114
