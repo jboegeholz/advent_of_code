2dfcb7fd2d83ebf7a4452a77d8570b0554ac4445
jboegeholz <boegeholz.joern@gmail.com>
2023-12-09 08:23:01 +0100

    add day 09

--- aoc_2023/day_09/test_aoc_day_09.py
+++ aoc_2023/day_09/test_aoc_day_09.py
@@ -49,6 +49,7 @@ def test_next_value_2():
     assert all_zero(differences_3)
     assert differences_3 == [0, 0, 0, 0]
 
+
     differences_2.append(differences_3[-1] + differences_2[-1])
     assert differences_2 == [1, 1, 1, 1, 1]
 
@@ -62,12 +63,7 @@ def test_next_value_3():
     line = [int(c) for c in "10 13 16 21 30 45".split(" ")]
     all_differences = []
     all_differences.append(line)
-    differences = []
-    for i in range(len(line)-1):
-        differences.append(line[i+1] - line[i])
-    all_differences.append(differences)
-    assert all_differences == [[10, 13, 16, 21, 30, 45], [3, 3, 5, 9, 15]]
-
+    # generate differences
     while not all_zero(all_differences[-1]):
         current_diff = all_differences[-1]
         differences = []
@@ -76,6 +72,16 @@ def test_next_value_3():
         all_differences.append(differences)
 
     assert all_differences == [[10, 13, 16, 21, 30, 45], [3, 3, 5, 9, 15], [0, 2, 4, 6], [2, 2, 2], [0, 0]]
+
+    all_differences[-1].append(0)
+
+    assert all_differences == [[10, 13, 16, 21, 30, 45], [3, 3, 5, 9, 15], [0, 2, 4, 6], [2, 2, 2], [0, 0, 0]]
+
+    for i in range(len(all_differences)-1, 0, -1):
+        current_diff = all_differences[i]
+        all_differences[i-1].append(current_diff[-1] + all_differences[i-1][-1])
+
+    assert all_differences == [[10, 13, 16, 21, 30, 45, 68], [3, 3, 5, 9, 15, 23], [0, 2, 4, 6, 8], [2, 2, 2, 2], [0, 0, 0]]
 
     #next_value = 0
     #assert next_value == 68
