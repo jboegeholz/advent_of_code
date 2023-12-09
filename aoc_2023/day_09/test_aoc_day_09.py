f71873f029a7e848a5b95fb0ddd9e72c6403fc7b
jboegeholz <boegeholz.joern@gmail.com>
2023-12-09 07:12:21 +0100

    add day 09

--- aoc_2023/day_09/test_aoc_day_09.py
+++ aoc_2023/day_09/test_aoc_day_09.py
@@ -18,3 +18,33 @@ def test_next_value():
 
     next_value = differences[-1] + line[-1]
     assert next_value == 18
+
+
+def test_next_value_2():
+    line = [int(c) for c in "1 3 6 10 15 21".split(" ")]
+    differences = []
+    for i in range(len(line)-1):
+        differences.append(line[i+1] - line[i])
+    assert differences == [2, 3, 4, 5, 6]
+
+    differences_2 = []
+    for i in range(len(differences)-1):
+        differences_2.append(differences[i+1] - differences[i])
+    assert differences_2 == [1, 1, 1, 1]
+
+    differences_3 = []
+    for i in range(len(differences_2)-1):
+        differences_3.append(differences_2[i+1] - differences_2[i])
+    assert differences_3 == [0, 0, 0]
+
+    differences_3.append(0)
+    assert differences_3 == [0, 0, 0, 0]
+
+    differences_2.append(differences_3[-1] + differences_2[-1])
+    assert differences_2 == [1, 1, 1, 1, 1]
+
+    differences.append(differences[-1] + differences_2[-1])
+
+    assert differences == [2,   3,   4,   5,   6,   7]
+    next_value = differences[-1] + line[-1]
+    assert next_value == 28
