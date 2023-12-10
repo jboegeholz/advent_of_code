4372691ce99e0c880bbe5213520c81297c1cac67
jboegeholz <boegeholz.joern@gmail.com>
2023-12-10 09:19:05 +0100

    add day 10

--- aoc_2023/day_10/test_aoc_day_10.py
+++ aoc_2023/day_10/test_aoc_day_10.py
@@ -4,16 +4,32 @@ data = """.....
 .L-J.
 ....."""
 
-
-def test_get_maze():
+def get_maze():
     maze = []
     for line in data.split("\n"):
         maze.append(list(line))
+    return maze
+
+def test_get_maze():
+    maze = get_maze()
     assert maze == [['.', '.', '.', '.', '.'],
                     ['.', 'S', '-', '7', '.'],
                     ['.', '|', '.', '|', '.'],
                     ['.', 'L', '-', 'J', '.'],
                     ['.', '.', '.', '.', '.']]
+
+
+def test_get_start_pos():
+    start_pos = ()
+    maze = get_maze()
+    for i, row in enumerate(maze):
+        for j, col in enumerate(row):
+            if col == 'S':
+                start_pos = (i, j)
+                break
+    assert start_pos == (1, 1)
+
+
 
 
 def test_get_max_steps():
