9fc92dddfa17c819e6f423412d20802a877f7f66
jobo8505 <Joern.Boegeholz@esolutions.de>
2024-12-02 08:36:33 +0100

    solve day 2 part 1

--- aoc_2024/day_02/test_aoc_day_01.py
+++ aoc_2024/day_02/test_aoc_day_01.py
@@ -11,34 +11,72 @@ def test_convert_to_lists():
 
     assert line == '1 3 6 7 9'
 
+
+def test_is_descending():
+    data = "7 6 4 2 1"
+    descending = True
+    values = [int(x) for x in data.split(" ")]
+    for i in range(len(values) - 1):
+        if values[i+1] > values[i]:
+            descending = False
+    assert descending
+
+def test_is_ascending():
+    data = "1 3 6 7 9"
+    ascending = True
+    values = [int(x) for x in data.split(" ")]
+    for i in range(len(values) - 1):
+        if values[i+1] < values[i]:
+            ascending = False
+    assert ascending
+
+def test_delta_between_one_and_three():
+    data = "1 3 6 7 9"
+    delta_ok = True
+    values = [int(x) for x in data.split(" ")]
+    for i in range(len(values) - 1):
+        if not 1 <= abs(values[i+1] - values[i]) <=3:
+            delta_ok = False
+    assert delta_ok
+
 def test_number_of_safe_reports():
     number_of_safe_reports = 0
     for line in data.split("\n"):
-        safe_report = True
-        values = line.split(" ")
+        ascending = True
+        descending = True
+        delta_ok = True
+        values = [int(x) for x in line.split(" ")]
         for i in range(len(values) - 1):
-            if (int(values[i+1]) <= int(values[i]) or
-               ((int(values[i+1]) - int(values[i]))) > 3):
-                safe_report = False
-                break
+            if values[i + 1] < values[i]:
+                ascending = False
+            if values[i + 1] > values[i]:
+                descending = False
+            if not 1 <= abs(values[i + 1] - values[i]) <= 3:
+                delta_ok = False
 
-        if safe_report:
+        if delta_ok and (ascending != descending):
             number_of_safe_reports += 1
 
     assert number_of_safe_reports == 2
 
 def test_number_of_safe_reports_full():
     number_of_safe_reports = 0
-    safe_report = True
-    with open("./aoc_data_01.txt", "r") as f:
 
+    with open("./aoc_data_01.txt", "r") as f:
         for line in f.readlines():
-            values = line.rstrip().split(" ")
+            ascending = True
+            descending = True
+            delta_ok = True
+            values = [int(x) for x in line.rstrip().split(" ")]
             for i in range(len(values) - 1):
-                if abs(int(values[i]) - int(values[i+1])) > 3:
-                    safe_report = False
-                    break
-            else:
+                if values[i + 1] < values[i]:
+                    ascending = False
+                if values[i + 1] > values[i]:
+                    descending = False
+                if not 1 <= abs(values[i + 1] - values[i]) <= 3:
+                    delta_ok = False
+
+            if delta_ok and (ascending != descending):
                 number_of_safe_reports += 1
 
     assert number_of_safe_reports == 2
