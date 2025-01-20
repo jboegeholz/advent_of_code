402bc4791fd988d0c9ed2a3e097c22758b746bb4
jboegeholz <boegeholz.joern@gmail.com>
2025-01-20 11:26:00 +0100

    add problem 003

---
+++ leetcode/003_longest_substring/test_longest_substring.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+def test_longest_substring():
+    s = "abcabcbb"
+    output = 0
+    assert output == 3
