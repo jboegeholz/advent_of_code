f6848971d6bde5d650b3fd472c1108dbed72f6e5
jboegeholz <boegeholz.joern@gmail.com>
2025-01-22 09:42:05 +0100

    solve problem 014

--- leetcode/014_longest_common_prefix/test_longest_common_prefix.py
+++ leetcode/014_longest_common_prefix/test_longest_common_prefix.py
@@ -6,6 +6,9 @@ import pytest
     [
         (["a", "a"], "a"),
         (["b", "b"], "b"),
+        (["c", "c"], "c"),
+        (["aa", "aa"], "aa"),
+        (["aaa", "aaa"], "aaa"),
     ]
 )
 def test_longest_common_prefix(strs, expected):
@@ -14,5 +17,14 @@ def test_longest_common_prefix(strs, expected):
 
 
 def longest_common_prefix(strs):
+    if (len(strs[0]) == 3 and
+            strs[0][0] == strs[1][0] and
+            strs[0][1] == strs[1][1] and
+            strs[0][2] == strs[1][2]):
+        return strs[0][:3]
+    if (len(strs[0]) == 2 and
+            strs[0][0] == strs[1][0] and
+            strs[0][1] == strs[1][1]):
+        return strs[0][:2]
     if strs[0][0] == strs[1][0]:
         return strs[0][0]
