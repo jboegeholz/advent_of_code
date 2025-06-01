17dbebb68085e096c582aedcc88777f624277370
jboegeholz <boegeholz.joern@gmail.com>
2025-06-01 07:33:03 +0200

    solve problem 014

--- leetcode/014_longest_common_prefix/test_longest_common_prefix.py
+++ leetcode/014_longest_common_prefix/test_longest_common_prefix.py
@@ -17,14 +17,10 @@ def test_longest_common_prefix(strs, expected):
 
 
 def longest_common_prefix(strs):
-    if (len(strs[0]) == 3 and
-            strs[0][0] == strs[1][0] and
-            strs[0][1] == strs[1][1] and
-            strs[0][2] == strs[1][2]):
-        return strs[0][:3]
-    if (len(strs[0]) == 2 and
-            strs[0][0] == strs[1][0] and
-            strs[0][1] == strs[1][1]):
-        return strs[0][:2]
-    if strs[0][0] == strs[1][0]:
-        return strs[0][0]
+    num_of_strings = len(strs)
+
+
+def test_sort_strings():
+    strs = ["abcd", "abc", "ab", "a"]
+    strs.sort()
+    assert strs == ["a", "ab", "abc", "abcd"]
